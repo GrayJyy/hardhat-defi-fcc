3281ee24efe2e44246911a63e5168bacb7e38a33
GrayJiang <Gray_Jy@yeah.net>
2023-05-03 20:35:21 +0800

    feat: daiPrice

--- help-hardhat-config.ts
+++ help-hardhat-config.ts
@@ -3,11 +3,13 @@ const networkConfig = {
     name: 'localhost',
     lendingPoolProviderAddress: '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
     IWethAddress: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // IWeth 主网地址 因为这里用的是主网forking
+    DaiToEth: '0x773616E4d11A78F511299002da57A0a94577F1f4',
   },
   11155111: {
     name: 'sepolia',
     lendingPoolProviderAddress: '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
-    IWethAddress: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    IWethAddress: '0x773616e4d11a78f511299002da57a0a94577f1f4',
+    DaiToEth: '0x773616E4d11A78F511299002da57A0a94577F1f4',
   },
 }
 
--- scripts/aaveBorrow.ts
+++ scripts/aaveBorrow.ts
@@ -5,6 +5,7 @@ import { networkConfig } from '../help-hardhat-config'
 import networkType from '../types/networkType'
 import getBorrowUserData from './getBorrowUserData'
 
+const chainId = network.config.chainId as networkType
 /*
 1.铸造流动性代币
 2.授权并存入流动性池
@@ -16,12 +17,16 @@ async function main() {
   await getWeth(deployer)
   const LendingPool = await getLendingPool(deployer)
   console.log(`LendingPool address is ${LendingPool.address}`)
-  const chainId = network.config.chainId as networkType
+
   const IWethAddress = networkConfig[chainId].IWethAddress
   await approveERC20(IWethAddress, LendingPool.address, AMOUNT, deploySigner)
   await LendingPool.deposit(IWethAddress, AMOUNT, deployer, 0)
   console.log('Deposited!')
   const { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(LendingPool, deployer)
+  const daiPrice = await getDaiPrice()
+  const availableBorrowsDai = (availableBorrowsETH.toString() as any) * 0.95 * (1 / daiPrice.toNumber()) // 0.95是为了留有余地
+  console.log(`You can borrow ${availableBorrowsDai.toString()} DAI`) // 用于展示
+  const amountDaiToBorrowWei = ethers.utils.parseEther(availableBorrowsDai.toString()) // 用于后续使用
 }
 
 const approveERC20 = async (erc20Address: string, spender: string, amount: any, deployer: any) => {
@@ -29,6 +34,12 @@ const approveERC20 = async (erc20Address: string, spender: string, amount: any, 
   const tx = await ERC20Token.approve(spender, amount)
   await tx.wait()
   console.log('Approved!')
+}
+const getDaiPrice = async () => {
+  const DaiToEthPriceFeed = await ethers.getContractAt('AggregatorV3Interface', networkConfig[chainId].DaiToEth)
+  const price = (await DaiToEthPriceFeed.latestRoundData())[1] // 可以点进去latestRoundData函数查看返回值，返回的是一个数组，数组里的第二个元素answer里有需要的数据
+  console.log(`Dai/Eth is ${price.toString()}`)
+  return price
 }
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
